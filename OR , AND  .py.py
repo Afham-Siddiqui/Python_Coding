a592cec08c64565cab728f403de8ca33e792b0de
Afham Siddiqui <62937387+Afham-Siddiqui@users.noreply.github.com>
2020-04-01 17:12:25 +0500

    Add files via upload

---
+++ OR , AND  .py.py
@@ -0,0 +1,9 @@
+username_set = "Afham"
+password_set = "635732"
+username_set_low = "afham"
+password_set = "12344"
+username = input("enter your name...")
+password = input("enter your password.....")
+
+if (username == username_set)or(username_set_low)and(password == password_set)or(password == password_set):
+    print("you have successfuly logged in!")
