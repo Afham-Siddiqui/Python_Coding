753d82ce4ab200786376b77d744904eb04301846
Afham Siddiqui <62937387+Afham-Siddiqui@users.noreply.github.com>
2020-04-01 22:22:36 +0500

    Add files via upload

---
+++ if(age).py
@@ -0,0 +1,13 @@
+N = str(input("enter your name...."))
+F = str(input("enter your father name...."))
+A = int(input("enter your age...."))
+print("you are",A,"years old")
+if A <= 10 :
+    print("you are in childhood")
+
+if A > 10 and  A <= 20 :
+    print("you are in teen age")
+
+if A > 20 and A <= 30 :
+    print("you are in adult age")
+
